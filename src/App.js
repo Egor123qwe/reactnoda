909e3809ca605af880adb39c542e7598ce6204c0
Yagor Truhan <48926720+Egor123qwe@users.noreply.github.com>
2022-08-16 18:05:13 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import './App.css';
-import ProfileContainer from './components/Profile/ProfileContainer';
+//import ProfileContainer from './components/Profile/ProfileContainer';
 import { Route, Routes } from 'react-router-dom';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
-import DialogsContainer from './components/Dialogs/DialogsContainer';
-import UsersContainer from './components/Users/UsersContainer';
+//import DialogsContainer from './components/Dialogs/DialogsContainer';
+//import UsersContainer from './components/Users/UsersContainer';
 import NavbarContainer from './components/Navbar/NavbarContainer';
 import HeaderConteiner from './components/Header/HeaderConteiner';
 import Login from './components/Login/Login';
-import React from 'react';
+import React, { Suspense } from 'react';
 import { connect } from 'react-redux';
 import { Initialize } from './redux/AppReducer';
 import Preloader from './components/Common/Preloader/Preloader';
+
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'))
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'))
+const UsersContainer = React.lazy(() => import('./components/Users/UsersContainer'))
+
 
 class AppMain extends React.Component {
 
@@ -29,16 +34,18 @@ class AppMain extends React.Component {
         <div className="App">
           <HeaderConteiner />
           <NavbarContainer />
-          <Routes>
-            <Route path='/login' element={<Login />}/>
-            <Route path='profile/' element={<ProfileContainer />}/>
-            <Route path='profile/:userId' element={<ProfileContainer />}/>
-            <Route path='/dialogs/*' element={<DialogsContainer />}/>
-            <Route path='/users/*' element={<UsersContainer />}/>
-            <Route path='/news' element={<News/>}/>
-            <Route path='/music' element={<Music/>}/>
-            <Route path='/settings' element={<Settings/>}/>
-          </Routes>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Routes>
+                <Route path='/login' element={<Login />}/>
+                <Route path='profile/' element={<ProfileContainer />}/>
+                <Route path='profile/:userId' element={<ProfileContainer />}/>
+                <Route path='/dialogs/*' element={<DialogsContainer />}/>
+                <Route path='/users/*' element={<UsersContainer />}/>
+                <Route path='/news' element={<News/>}/>
+                <Route path='/music' element={<Music/>}/>
+                <Route path='/settings' element={<Settings/>}/>
+              </Routes>
+            </Suspense>
         </div>
       )
     }
