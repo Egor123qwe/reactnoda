366657d7e4cba248bf3fef95030301b37c61aaca
Yagor Truhan <48926720+Egor123qwe@users.noreply.github.com>
2022-08-16 06:21:39 +0300

    Updates

---
+++ precache-manifest.17de83cf72516cd83413a6d6b081919e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3bc6c934eeb97caffe4b",
+    "url": "/reactnoda/static/css/main.f6ad4958.chunk.css"
+  },
+  {
+    "revision": "3bc6c934eeb97caffe4b",
+    "url": "/reactnoda/static/js/main.40f5d997.chunk.js"
+  },
+  {
+    "revision": "8e08eba36ecb3afa6f2c",
+    "url": "/reactnoda/static/js/runtime~main.5ff26b87.js"
+  },
+  {
+    "revision": "ee4e79028d32bb2108d7",
+    "url": "/reactnoda/static/js/2.8bf4821a.chunk.js"
+  },
+  {
+    "revision": "a18b610acfe131f2d474",
+    "url": "/reactnoda/static/js/3.49b3ba72.chunk.js"
+  },
+  {
+    "revision": "6402ba95681ad29313eb116656c4cb4f",
+    "url": "/reactnoda/static/media/Preloader.6402ba95.svg"
+  },
+  {
+    "revision": "62a6f314ae441befb2f4573dd33c0b9e",
+    "url": "/reactnoda/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ReactDevTest/precache-manifest.8341e3b9c2a76bc1f6f0fcff7a298e6a.js"
+  "/reactnoda/precache-manifest.17de83cf72516cd83413a6d6b081919e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ReactDevTest/index.html", {
+workbox.routing.registerNavigationRoute("/reactnoda/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
