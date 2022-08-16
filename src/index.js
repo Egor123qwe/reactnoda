0fd4e6e7c473d6968f51f18d237daf85317a0389
Yagor Truhan <48926720+Egor123qwe@users.noreply.github.com>
2022-08-16 06:40:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
         <App state = {store.getState()} dispatch={store.dispatch.bind(store)}/>
       </Provider>
