33e7471bf2ba82a18eb52f731936a3e476e52628
Yagor Truhan <48926720+Egor123qwe@users.noreply.github.com>
2022-08-16 06:20:17 +0300

    Updates

---
+++ precache-manifest.8341e3b9c2a76bc1f6f0fcff7a298e6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3bc6c934eeb97caffe4b",
+    "url": "/ReactDevTest/static/css/main.f6ad4958.chunk.css"
+  },
+  {
+    "revision": "3bc6c934eeb97caffe4b",
+    "url": "/ReactDevTest/static/js/main.40f5d997.chunk.js"
+  },
+  {
+    "revision": "bc04b71690bcbcf5c853",
+    "url": "/ReactDevTest/static/js/runtime~main.7cebe0bc.js"
+  },
+  {
+    "revision": "ee4e79028d32bb2108d7",
+    "url": "/ReactDevTest/static/js/2.8bf4821a.chunk.js"
+  },
+  {
+    "revision": "a18b610acfe131f2d474",
+    "url": "/ReactDevTest/static/js/3.49b3ba72.chunk.js"
+  },
+  {
+    "revision": "6402ba95681ad29313eb116656c4cb4f",
+    "url": "/ReactDevTest/static/media/Preloader.6402ba95.svg"
+  },
+  {
+    "revision": "73b23905860492444d823dcec00d7e23",
+    "url": "/ReactDevTest/index.html"
+  }
+];
