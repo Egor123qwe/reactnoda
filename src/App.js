3e21f119843a614721ac7a8f9b6f34c2dc5ce258
Yagor Truhan <48926720+Egor123qwe@users.noreply.github.com>
2022-08-16 18:11:34 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
-//import ProfileContainer from './components/Profile/ProfileContainer';
 import { Route, Routes } from 'react-router-dom';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
-//import DialogsContainer from './components/Dialogs/DialogsContainer';
-//import UsersContainer from './components/Users/UsersContainer';
 import NavbarContainer from './components/Navbar/NavbarContainer';
 import HeaderConteiner from './components/Header/HeaderConteiner';
 import Login from './components/Login/Login';
